77503bde4b3b3139c747c4b87076931fea73b9df
Sahil Uppal <sahiluppal2k@gmail.com>
2020-01-26 12:17:09 +0530

    argparser included

--- yolov3/detect_image.py
+++ yolov3/detect_image.py
@@ -7,6 +7,7 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from matplotlib import pyplot
 from matplotlib.patches import Rectangle
+import argparse
  
 class BoundBox:
 	def __init__(self, xmin, ymin, xmax, ymax, objness = None, classes = None):
@@ -159,54 +160,73 @@ def draw_boxes(filename, v_boxes, v_labels, v_scores):
 		# calculate width and height of the box
 		width, height = x2 - x1, y2 - y1
 		# create the shape
-		rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+		rect = Rectangle((x1, y1), width, height, fill=False, color='green')
 		# draw the box
 		ax.add_patch(rect)
 		# draw text and score in top left corner
 		label = "%s (%.3f)" % (v_labels[i], v_scores[i])
-		pyplot.text(x1, y1, label, color='red')
+		pyplot.text(x1, y1, label, color='green')
 	# show the plot
 	pyplot.show()
- 
-# load yolov3 model
-model = load_model('model.h5')
-# define the expected input shape for the model
-input_w, input_h = 416, 416
-# define our new photo
-photo_filename = 'city.jpeg'
-# load and prepare image
-image, image_w, image_h = load_image_pixels(photo_filename, (input_w, input_h))
-# make prediction
-yhat = model.predict(image)
-# summarize the shape of the list of arrays
-print([a.shape for a in yhat])
-# define the anchors
-anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
-# define the probability threshold for detected objects
-class_threshold = 0.6
-boxes = list()
-for i in range(len(yhat)):
-	# decode the output of the network
-	boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, input_h, input_w)
-# correct the sizes of the bounding boxes for the shape of the image
-correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
-# suppress non-maximal boxes
-do_nms(boxes, 0.5)
-# define the labels
-labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
-	"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
-	"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
-	"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
-	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
-	"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
-	"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
-	"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
-	"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
-	"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
-# get the details of the detected objects
-v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
-# summarize what we found
-for i in range(len(v_boxes)):
-	print(v_labels[i], v_scores[i])
-# draw what we found
-draw_boxes(photo_filename, v_boxes, v_labels, v_scores)
+
+
+def main(filename):
+	model = load_model('model.h5')
+	input_w, input_h = 416, 416
+
+	photo_filename = filename
+
+	image, image_w, image_h = load_image_pixels(photo_filename, (input_w, input_h))
+
+	yhat = model.predict(image)
+	print([a.shape for a in yhat])
+	anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
+
+	class_threshold = 0.6
+	boxes = list()
+
+	for i in range(len(yhat)):
+		boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, input_h, input_w)
+
+	# correct the sizes of the bounding boxes for the shape of the image
+	correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
+
+	do_nms(boxes, 0.5)
+
+	labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+		"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+		"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+		"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+		"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+		"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+		"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+		"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
+		"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+		"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+	v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
+	# summarize what we found
+	for i in range(len(v_boxes)):
+		print(v_labels[i], v_scores[i])
+	# draw what we found
+
+	draw_boxes(photo_filename, v_boxes, v_labels, v_scores)
+
+argparser = argparse.ArgumentParser(
+	description='Object Detection in images'
+)
+
+argparser.add_argument(
+	'-i',
+	'--image',
+	help='path to image file'
+)
+
+
+if __name__=='__main__':
+	args = argparser.parse_args()
+	if args.image:
+		main(args.image)
+	else:
+		print('No image path specified. Defaults for testing')
+		main('car.jpeg')
