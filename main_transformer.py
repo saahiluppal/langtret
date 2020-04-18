15e05d1f4f7af649f08366a753a34a030e14bd57
Sahil Uppal <sahiluppal2k@gmail.com>
2020-04-18 10:53:38 +0530

    enabled tags

--- main_transformer.py
+++ main_transformer.py
@@ -5,21 +5,25 @@ from tqdm import tqdm
 from keras_utils import (CustomSchedule,
                          create_look_ahead_mask,
                          create_padding_mask)
+from absl import app, flags
 import time
 import io
 import numpy as np
 import re
 
-PATH = './fra.txt'
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('path', None, 'Path to dataset')
+flags.DEFINE_integer('batch', 64, 'Batch Size')
+flags.DEFINE_integer('sample', 150_000, 'No of lines to train on')
+flags.DEFINE_integer('patience', 5, 'Patience for early stopping')
+flags.DEFINE_integer('epochs', 100, 'No of Epochs')
 BUFFER_SIZE = 420_000
-BATCH_SIZE = 64
 EMBEDDING_DIM = 512
 NUM_LAYERS = 6
 PATIENCE = 5
 DFF = 2048
 NUM_HEADS = 8
-EXAMPLES = 150_000
-EPOCHS = 100
 
 
 def preprocess(sentence, lower=False):
@@ -106,97 +110,100 @@ def loss_function(real, pred, obj):
 
     return tf.reduce_sum(loss_)/tf.reduce_sum(mask)
 
+def main(absl):
+    lang1, lang2, tok1, tok2 = load_dataset(FLAGS.path, num_examples=FLAGS.sample)
+    dataset = tf.data.Dataset.from_tensor_slices((lang1, lang2))
 
-lang1, lang2, tok1, tok2 = load_dataset(PATH, num_examples=EXAMPLES)
-dataset = tf.data.Dataset.from_tensor_slices((lang1, lang2))
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(FLAGS.batch,
+                                                drop_remainder=True)
 
-dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE,
-                                             drop_remainder=True)
+    input_vocab_size = tok1.vocab_size + 2
+    target_vocab_size = tok2.vocab_size + 2
+    dropout_rate = 0.1
 
-input_vocab_size = tok1.vocab_size + 2
-target_vocab_size = tok2.vocab_size + 2
-dropout_rate = 0.1
+    learning_rate = CustomSchedule(EMBEDDING_DIM)
+    optimizer = tf.keras.optimizers.Adam(learning_rate,
+                                        beta_1=0.9, beta_2=0.98, epsilon=1e-9)
 
-learning_rate = CustomSchedule(EMBEDDING_DIM)
-optimizer = tf.keras.optimizers.Adam(learning_rate,
-                                     beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none'
+    )
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+        name='train_accuracy')
 
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-    from_logits=True, reduction='none'
-)
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-    name='train_accuracy')
+    transformer = Transformer(NUM_LAYERS, EMBEDDING_DIM, NUM_HEADS, DFF,
+                            input_vocab_size, target_vocab_size,
+                            pe_input=input_vocab_size,
+                            pe_target=target_vocab_size,
+                            rate=dropout_rate)
 
-transformer = Transformer(NUM_LAYERS, EMBEDDING_DIM, NUM_HEADS, DFF,
-                          input_vocab_size, target_vocab_size,
-                          pe_input=input_vocab_size,
-                          pe_target=target_vocab_size,
-                          rate=dropout_rate)
+    checkpoint_path = "./checkpoints/train"
+    ckpt = tf.train.Checkpoint(transformer=transformer)
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=3)
 
-checkpoint_path = "./checkpoints/train"
-ckpt = tf.train.Checkpoint(transformer=transformer)
-ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print('Latest checkpoint restored!!')
 
-if ckpt_manager.latest_checkpoint:
-    ckpt.restore(ckpt_manager.latest_checkpoint)
-    print('Latest checkpoint restored!!')
-
-loss_history = []
+    loss_history = []
 
 
-@tf.function
-def train_step(inp, tar):
-    tar_inp = tar[:, :-1]
-    tar_real = tar[:, 1:]
+    @tf.function
+    def train_step(inp, tar):
+        tar_inp = tar[:, :-1]
+        tar_real = tar[:, 1:]
 
-    enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
-        inp, tar_inp)
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
+            inp, tar_inp)
 
-    with tf.GradientTape() as tape:
-        predictions, _ = transformer(inp, tar_inp,
-                                     True,
-                                     enc_padding_mask,
-                                     combined_mask,
-                                     dec_padding_mask)
-        loss = loss_function(tar_real, predictions, loss_object)
+        with tf.GradientTape() as tape:
+            predictions, _ = transformer(inp, tar_inp,
+                                        True,
+                                        enc_padding_mask,
+                                        combined_mask,
+                                        dec_padding_mask)
+            loss = loss_function(tar_real, predictions, loss_object)
 
-    gradients = tape.gradient(loss, transformer.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
 
-    train_loss(loss)
-    train_accuracy(tar_real, predictions)
+        train_loss(loss)
+        train_accuracy(tar_real, predictions)
 
-try:
-    print("Training Start...")
-    for epoch in range(EPOCHS):
-        start = time.time()
+    try:
+        print("Training Start...")
+        for epoch in range(FLAGS.epochs):
+            start = time.time()
 
-        train_loss.reset_states()
-        train_accuracy.reset_states()
+            train_loss.reset_states()
+            train_accuracy.reset_states()
 
-        print(f'Epoch {epoch + 1} Started...')
-        for batch, (inp, tar) in enumerate(dataset):
-            train_step(inp, tar)
-            print(f'Batch: {batch}', end='\r')
-        print(f'Time {time.time() - start}')
-        print(f'Epoch {epoch + 1}, Loss: {train_loss.result()}, Accuracy: {train_accuracy.result()}\n')
+            print(f'Epoch {epoch + 1} Started...')
+            for inp, tar in dataset:
+                train_step(inp, tar)
+                print('.', end='')
+            print(f'\nTime {time.time() - start}')
+            print(f'Epoch {epoch + 1}, Loss: {train_loss.result()}, Accuracy: {train_accuracy.result()}\n')
 
-        if (epoch + 1) % 10 == 0:
-            ckpt_save_path = ckpt_manager.save()
-            print('Saving Checkpoint')
+            if (epoch + 1) % 10 == 0:
+                ckpt_manager.save()
+                print('Saving Checkpoint')
 
-        loss_history.append(train_loss.result())
-        low = len(np.where(np.array(loss_history) < train_loss.result())[0])
-        if low >= PATIENCE:
-            print("Early Stopping...")
-            break
-except KeyboardInterrupt:
+            loss_history.append(train_loss.result())
+            low = len(np.where(np.array(loss_history) < train_loss.result())[0])
+            if low >= PATIENCE:
+                print("Early Stopping...")
+                break
+    except KeyboardInterrupt:
+        tok1.save_to_file('tok_lang1')
+        tok2.save_to_file('tok_lang2')
+        ckpt_manager.save()
+
+    print("Training End...")
     tok1.save_to_file('tok_lang1')
     tok2.save_to_file('tok_lang2')
     ckpt_manager.save()
 
-print("Training End...")
-tok1.save_to_file('tok_lang1')
-tok2.save_to_file('tok_lang2')
-ckpt_manager.save()
+if __name__ == '__main__':
+    app.run(main)
